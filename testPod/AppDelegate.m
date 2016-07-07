9de4bae5c00f6687defa602e9ff548580716f5f0
shaoyu <shaoyu_yang@mengbaby.com>
2016-07-07 16:43:02 +0800

    234

--- testPod/AppDelegate.m
+++ testPod/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    return NO;
+    return YES;
     
 }
 
@@ -41,6 +41,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"====");
 }
 
 @end
